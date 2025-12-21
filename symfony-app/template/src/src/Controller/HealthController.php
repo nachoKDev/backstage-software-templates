eb26244eaa2b5ee02ce1ebdfe13c687a43b98657
nachoKDev <nachokorkk@gmail.com>
2025-12-21 14:54:45 +0100

    doc openapi

--- symfony-app/template/src/src/Controller/HealthController.php
+++ symfony-app/template/src/src/Controller/HealthController.php
@@ -1,12 +1,29 @@
-// src/Controller/HealthController.php
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HealthController
+class HealthController extends AbstractController
 {
-    #[Route('/api/v1/healthz', methods: ['GET'])]
+    #[Route('/api/v1/healthz', name: 'api_healthz', methods: ['GET'])]
     public function health(): JsonResponse
     {
-        return new JsonResponse(['status' => 'ok']);
+        return $this->json([
+            'status' => 'ok',
+        ]);
+    }
+
+    #[Route('/api/v1/info', name: 'api_info', methods: ['GET'])]
+    public function info(): JsonResponse
+    {
+        return $this->json([
+            'service' => 'example-api',
+            'version' => '1.0.0',
+            'environment' => $_ENV['APP_ENV'] ?? 'unknown',
+            'timestamp' => (new \DateTime())->format(\DateTime::ATOM),
+        ]);
     }
 }
